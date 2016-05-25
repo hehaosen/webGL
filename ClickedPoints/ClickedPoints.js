f8bda4865880981dab3f8f432f3291b56d20acae
hehaosen <307052084@qq.com>
2016-05-25 16:57:10 +0800

    +ClickedPoints

---
+++ ClickedPoints/ClickedPoints.js
@@ -0,0 +1,67 @@
+// ClickedPoints.js
+// 顶点着色器
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+    '   gl_Position = a_Position;\n' +
+    '   gl_PointSize = 10.0;\n' +
+    '}\n';
+
+// 片元着色器程序
+var FSHADER_SOURCE =
+    'void main() {\n' +
+    ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // 设置颜色
+    '}\n';
+
+function main () {
+    // 获取<canvas>元素
+    var canvas = document.getElementById('webgl');
+
+    // 获取webGL上下文
+    var gl =getWebGLContext(canvas);
+
+    if(!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    // 初始化着色器
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders');
+        return;
+    }
+
+    // 获取a_Position变量的存储位置
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+    // 注册鼠标点击事件响应函数
+    canvas.onmousedown = function (ev) { click(ev, gl, canvas, a_Position);};
+
+    // 清空<canvas>的背景色
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+var g_points = []; //鼠标点击位置数组
+function click (ev, gl, canvas, a_Position) {
+    var x = ev.clientX; //鼠标点击处的x坐标
+    var y = ev.clientY; // 鼠标点击处的y坐标
+    var rect = ev.target.getBoundingClientRect();
+    x = ((x - rect.left) - canvas.height/2)/(canvas.height/2);
+    y = (canvas.width/2 - (y - rect.top))/(canvas.width/2);
+
+    // 将坐标存储到g_points数组里
+    g_points.push(x); g_points.push(y);
+
+    //清除<canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var len = g_points.length;
+    for (var i = 0; i<len ; i+=2) {
+        // 将点的位置传递到变量中a_Position
+        gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+        //绘制点
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+}
--- HelloPoint2/HelloPoint2.js
+++ HelloPoint2/HelloPoint2.js
@@ -3,9 +3,10 @@
 // 顶点着色器
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
+    'attribute float a_PointSize;\n' +
     'void main() {\n' +
     '   gl_Position = a_Position;\n' +
-    '   gl_PointSize = 10.0;\n' +
+    '   gl_PointSize = a_PointSize;\n' +
     '}\n';
 
 // 片元着色器程序
@@ -34,6 +35,7 @@ function main() {
 
     // 获取attribute变量的存储位置
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
 
     if (a_Position < 0) {
         console.log('Failed to get the storage location of a_Position');
@@ -42,6 +44,7 @@ function main() {
 
     // 将顶点位置传输给attribute变量
     gl.vertexAttrib3f(a_Position, 0.0, 0.0, 1.0);
+    gl.vertexAttrib1f(a_PointSize, 10.0);
 
     // 清空<canvas>的背景色
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
