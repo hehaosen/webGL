b62e628e6e93f1289b6ba6af58470713150b078d
hehaosen <307052084@qq.com>
2016-05-25 14:13:14 +0800

    +HelloPoint2

--- HelloPoint2/HelloPoint2.js
+++ HelloPoint2/HelloPoint2.js
@@ -1,10 +1,11 @@
-// HelloPoint1.js
+// HelloPoint2.js
 
-// 顶点着色器程序
+// 顶点着色器
 var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
     'void main() {\n' +
-    ' gl_Position = vec4(0.5, 0.0, 0.0, 1.0);\n' + // 设置坐标
-    ' gl_PointSize = 10.0;\n' + // 设置尺寸
+    '   gl_Position = a_Position;\n' +
+    '   gl_PointSize = 10.0;\n' +
     '}\n';
 
 // 片元着色器程序
@@ -13,13 +14,12 @@ var FSHADER_SOURCE =
     ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // 设置颜色
     '}\n';
 
-
 function main() {
     // 获取<canvas>元素
     var canvas = document.getElementById('webgl');
 
-    // 获取WebGL绘图上下文
-    var gl = getWebGLContext(canvas);
+    // 获取webGL上下文
+    var gl =getWebGLContext(canvas);
 
     if(!gl) {
         console.log('Failed to get the rendering context for WebGL');
@@ -32,7 +32,18 @@ function main() {
         return;
     }
 
-    // 设置<canvas>的背景色
+    // 获取attribute变量的存储位置
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    // 将顶点位置传输给attribute变量
+    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 1.0);
+
+    // 清空<canvas>的背景色
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     // 清空<canvas>
@@ -40,4 +51,5 @@ function main() {
 
     // 绘制一个点
     gl.drawArrays(gl.POINTS, 0, 1);
+
 }
